05505952cc76e99106bbba2fed50c9c98d51df59
Justin Liu <yuting1987@gmail.com>
2014-04-17 18:22:13 +0800

    刪除安全判斷

--- index.js
+++ index.js
@@ -13,16 +13,13 @@ this.server = http.createServer( function( req, res ) {
   }
 
   req.on( "end", function() {
-    if ( _.indexOf( gith.ips, req.connection.remoteAddress ) >= 0 ||
-         _.indexOf( gith.ips, "*" ) >= 0 ) {
-      if ( /^payload=/.test( data ) ) {
-        var payload = JSON.parse( querystring.unescape(data.slice(8)) );
-        console.log(payload);
-        res.writeHead( 200, {
-          'Content-type': 'text/html'
-        });
-      }
-    }      
+    if ( /^payload=/.test( data ) ) {
+      var payload = JSON.parse( querystring.unescape(data.slice(8)) );
+      console.log(payload);
+      res.writeHead( 200, {
+        'Content-type': 'text/html'
+      });
+    }
     res.end();
   });
 
