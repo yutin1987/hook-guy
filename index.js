aef4e4535fe06e212625976cf88e10884d4b07d4
Justin Liu <yuting1987@gmail.com>
2014-04-18 17:55:10 +0800

    add exists判斷

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var http = require("http");
 var util = require("util");
 var querystring = require("querystring");
 var git = require('simple-git');
+var async = require('async');
+var fs = require('fs');
 
 var port = 8000;
 
@@ -21,9 +23,22 @@ this.server = http.createServer( function( req, res ) {
     if ( /^0+$/.test(payload.after) ){
     }else{
       var ref = payload.ref;
-      var branch = ref.substr(ref.lastIndexOf('/')+1)
-      var rep = git('/var/www/html/'+branch);
-      rep.clone(payload.repository.url, './', function(){
+      var branch = ref.substr(ref.lastIndexOf('/')+1);
+      var path = '/var/www/html/'+branch;
+      var rep;
+      async.series([
+        function (cb) {
+          fs.exists(path, function (exists) { cb(exists ? true : null)});
+        },
+        function (cb) {
+          fs.mkdir(path, 777, cb); 
+        },
+        function (cb) {
+          rep = git('/var/www/html/'+branch);
+          rep.clone(payload.repository.url, './', cb);
+        }
+      ], function () {
+        rep = git('/var/www/html/'+branch);
         rep.checkout(branch, function(){
           rep.pull();
         })
