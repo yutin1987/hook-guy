aa659045a8ba834d73705a185e25ea6161abdc37
Justin Liu <yuting1987@gmail.com>
2014-04-22 17:41:59 +0800

    0.1.5

--- index.js
+++ index.js
@@ -10,14 +10,66 @@ var http = require("http"),
 var program = require('commander');
 var exec = require('child_process').exec;
 
+var nginx = function(){/*
+server {
+    listen         80;
+    server_name    ${virtual_host};
+    server_tokens  off;
+    access_log     /var/log/nginx/${virtual_host}.access.log;
+    root           /var/www/html/${virtual_host}/public;
+
+    location / {
+        index       index.php;
+        add_header  X-Content-Type-Options "nosniff";
+        add_header  X-XSS-Protection "1; mode=block";
+        if (!-f $request_filename) {
+            rewrite ^(.+)$ /index.php?_url=$1 last;
+        }
+    }
+
+    location ~ \.php$ {
+        fastcgi_pass   127.0.0.1:9000;
+        fastcgi_index  index.php;
+        include        /etc/nginx/fastcgi_params;
+        fastcgi_param  SCRIPT_FILENAME  $document_root/$fastcgi_script_name;
+        fastcgi_param  APPLICATION_ENV development;
+    }
+
+    location ~ \.(js|ico|gif|jpg|png|css|phtml)$ {
+        add_header     X-Content-Type-Options "nosniff";
+        add_header     X-XSS-Protection "1; mode=block";
+        access_log     off;
+        log_not_found  off;
+    }
+
+    location = /favicon.ico {
+        access_log     off;
+        log_not_found  off;
+    }
+
+    location ~ /\. {
+        access_log     off;
+        log_not_found  off;
+        deny           all;
+    }
+}
+*/}.toString().slice(14,-3);
+
 program
   .version('0.0.1')
   .option('-p, --port [port]', 'Port')
   .option('-d, --dir [path]', '/var/www/html')
   .option('-v, --vhost [host]', 'Virtual Host')
   .option('-n, --nginx [path]', 'Nginx Path')
-  .option('-c, --config [file]', 'Nginx Config')
+  .option('-s, --sites [file]', 'Nginx Config')
+  .option('-c, --config', 'Create Config')
   .parse(process.argv);
+
+if (program.config) {
+  fs.writeFileSync('nginx', nginx);
+  console.log('create nginx configs');
+  process.exit(0);
+}
 
 this.server = http.createServer( function( req, res ) {
   console.log(req.method);
@@ -35,9 +87,12 @@ this.server = http.createServer( function( req, res ) {
     var branch = ref.substr(ref.lastIndexOf('/')+1);
     var vhost = branch + (program.vhost ? '.' + program.vhost : '');
     var path = '/var/www/html/' + vhost;
+    var nginx = program.nginx || '/etc/nginx/sites-available';
     console.log('updated ' + vhost);
     if ( /^0+$/.test(payload.after) ){
-      fs.unlink(path);
+      exec('rm -rf ' + nginx + '/' + vhost);
+      exec('rm -rf ' + path);
+      exec('sudo service nginx reload');
     }else{
       var rep;
       async.series([
@@ -55,7 +110,6 @@ this.server = http.createServer( function( req, res ) {
         },
         function (cb) {
           console.log('set config ' + vhost);
-          var nginx = program.nginx || '/etc/nginx/sites-available';
           var config = program.config;
           if (config && fs.existsSync(config)){
             config = fs.readFileSync(config).toString();
